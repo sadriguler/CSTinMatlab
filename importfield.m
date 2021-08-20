001f9383d37e2228d6a2c3626cba632e961a2af1
Sadri Guler <sadriguler@protonmail.com>
2021-08-20 11:15:17 +0200

    addition of data locs

--- importfield.m
+++ importfield.m
@@ -1,5 +1,6 @@
-function thefield = importfield(filename)
+function [theloc, thefield] = importfield(filename)
 thedata = importdata(filename);
 thedata  = thedata.data;
 thefield = [thedata(:,4) + 1i*thedata(:,5),thedata(:,6) + 1i*thedata(:,7),thedata(:,8) + 1i*thedata(:,9)];
+theloc = thedata(:,1:3);
 end
