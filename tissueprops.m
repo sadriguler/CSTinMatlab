5e433efdcfc49f5792d1a900a2391881a3383cf3
Sadri Guler <sadriguler@protonmail.com>
2022-02-21 13:12:05 +0100

    tissueprops
    more tissue props are added

--- tissueprops.m
+++ tissueprops.m
@@ -3,4 +3,9 @@ thedata = importdata(filename);
 thecmap = thedata.data(:,10:12)/255;
 electcond = thedata.data(:,4);
 density = thedata.data(:,5);
+
+% add air
+thecmap = [0 0 0;thecmap];
+electcond = [0; electcond];
+density = [0; density];
 end
